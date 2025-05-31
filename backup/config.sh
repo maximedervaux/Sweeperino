04ab5f9f2170d90176ef4cc8269eee3e7784c356
UgoW <ugo.warembourg@student.junia.com>
2025-05-31 23:36:45 +0200

    refactor(BACKUP features): Merge BACKUP's config into MAIN config file

--- backup/config.sh
+++ backup/config.sh
@@ -2,16 +2,3 @@
 
 # Configuration file 
 
-# Target directory to backup
-SOURCE_DIR="$HOME/Documents"
-
-# Backup directory
-BACKUP_DIR="$HOME/backups"
-
-# (to implement) Interval for automatic backups in minute
-INTERVAL_MINUTES=30
-
-# MAX number of backups to keep
-MAX_BACKUPS=5
-
-ARGON_SALT="12345678"
