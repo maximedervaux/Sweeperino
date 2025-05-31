8cbe823e744ac6951179503e531d6e547c536dc6
Williampasset <william.passet@student.junia.com>
2025-05-31 17:14:52 +0200

    feat(packets-cleaner) : add packets-cleaner file and move log function to utils

--- fonctions/logs-cleaner.sh
+++ fonctions/logs-cleaner.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-#Fonction de log générique
-log() {
-    # Définit le type de message (par défaut "INFO")
-    local msg_type="${1:-INFO}"
-
-    # Définit le contenu du message, ou un message vide si non fourni
-    local message="${2:-}"
-
-    # Affiche et enregistre le message dans le fichier de log
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$msg_type] $message" | tee -a "$LOG_FILE"
-}
+source "$SCRIPT_DIR/utils/log.sh"
 
 #Nettoyage des logs vieux de 30 jours
 nettoyer_logs() {
