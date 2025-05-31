d99938ae5d2ed862faf680abd9293f198f7208cd
UgoW <ugo.warembourg@student.junia.com>
2025-05-31 23:30:58 +0200

    docs(BACKUP features): :bulb: Add restore.sh comment
    Add document to restore.sh to improve reading quality

--- backup/restore.sh
+++ backup/restore.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# === Chargement de la configuration ===
+# Load configuration
 source "$(dirname "$0")/config.sh"
 
-# === Choix de l'archive à restaurer ===
+# Choose the backup archive to restore
+# (Merge with existing the interface)
 cd "$BACKUP_DIR" || exit 1
 echo "Archives disponibles :"
 select ARCHIVE in backup_*.enc; do
     [ -n "$ARCHIVE" ] && break
 done
 
-# === Mot de passe ===
+# Password input
 read -s -p "Mot de passe : " PASSWORD
 echo
 KEY=$(echo -n "$PASSWORD" | argon2 "$ARGON_SALT" -id -t 2 -m 16 -p 1 -l 32 -r | awk '{print $2}')
 
-# === Déchiffrement ===
+# Decrypt
 DECRYPTED="/tmp/restore.tar.gz"
 openssl enc -d -aes-256-cbc -pbkdf2 -in "$ARCHIVE" -out "$DECRYPTED" -pass pass:"$KEY" || {
     echo "Échec du déchiffrement."
     exit 1
 }
 
-# === Extraction ===
+# EXtract the archive and put it in /tmp/restore
 mkdir -p /tmp/restore
 tar -xzf "$DECRYPTED" -C /tmp/restore
 echo "Sauvegarde extraite dans /tmp/restore"
