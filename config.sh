602c7943e5d11429eed26712be8d2212dd911271
Max D <82460842+maximedervaux@users.noreply.github.com>
2025-06-02 12:03:51 +0200

    Update config.sh with sort config

--- config.sh
+++ config.sh
@@ -14,3 +14,12 @@ INTERVAL_MINUTES=30
 
 # MAX number of backups to keep
 MAX_BACKUPS=5
+
+# --- CONFIG SORT ---
+SOURCE_DIR_SORT="$(xdg-user-dir DOWNLOAD 2>/dev/null || echo "$HOME/Downloads")" 
+USER_DESKTOP_DIR="$(xdg-user-dir DESKTOP 2>/dev/null || echo "$HOME/Desktop")"
+USER_DOCUMENTS_DIR="$(xdg-user-dir DOCUMENTS 2>/dev/null || echo "$HOME/Documents")"
+USER_DOWNLOAD_DIR="$SOURCE_DIR" 
+USER_MUSIC_DIR="$(xdg-user-dir MUSIC 2>/dev/null || echo "$HOME/Music")"
+USER_PICTURES_DIR="$(xdg-user-dir PICTURES 2>/dev/null || echo "$HOME/Pictures")"
+USER_VIDEOS_DIR="$(xdg-user-dir VIDEOS 2>/dev/null || echo "$HOME/Videos")"
